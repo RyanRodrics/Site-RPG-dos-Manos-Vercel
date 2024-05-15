3e28aa7a86826598c3829bade14eb7680a00865e
Andrededede <andrelucas1708@gmail.com>
2024-05-15 17:30:22 -0300

    umas coisa ai no index

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,21 @@
 import { navSelect } from "./header.js";
 navSelect("");
+
+const campanhas = document.querySelectorAll("#campanhas li");
+
+campanhas.forEach((element,index) =>{
+    element.onmouseover = () => {
+        
+        campanhas.forEach((resto) =>{
+            resto.style.width = `${ (100-(100/campanhas.length)*2)/(campanhas.length-1)}%`;
+        })
+        campanhas[index].style.width = `${(100/campanhas.length)*2}%`;
+        campanhas[index].style.filter = "brightness(1) blur(0)";
+    }
+    element.onmouseout = () =>{
+        campanhas.forEach((todos) =>{
+            todos.style.width = `${100/campanhas.length}%`;
+        })
+        campanhas[index].style.filter = "brightness(0.5) blur(1px)";
+    }
+})
